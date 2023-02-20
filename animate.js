8d8a72f07e1d515c34bff04a985d56403e3e2ad5
AaaXya <2939166862@qq.com>
2023-02-21 01:16:33 +0800

    update:openSwing

--- animate.js
+++ animate.js
@@ -4,51 +4,41 @@ class CreatePattern {
         this.x = x
         this.y = y
         this.options = {
-            ...{
-                type: 'aixin', //class属性名
-                quantity: 10, //数量
-                distanceMax: 100, //移动最大距离px
-                distanceMin: 20,
-                palette: ['f9f383', 'eb125f', '6eff8a', '66ffff'], //调色盘
-            },
+            type: 'aixin',
+            quantity: 10,
+            distanceMax: 100,
+            distanceMin: 20,
+            palette: ['f9f383', 'eb125f', '6eff8a', '66ffff'],
             ...config,
         }
-        this.openSwing = () =>
-            (Math.random() < 0.5 ? -1 : 1) *
-            //[n,m] 范围随机数 Math.floor(Math.random() * (m - n + 1)) + n;
-            (this.mFmR(this.options.distanceMax - this.options.distanceMin + 1) +
-                this.options.distanceMin)
         this.growUp()
     }
 
-    //添加属性
     giveBirthToAChild() {
         const initialStyles = document.createElement('i')
-        initialStyles.style.cssText = `background-color:#${
-            this.options.palette[this.mFmR(this.options.palette.length)]
-        };left:${this.x}px;top:${this.y}px;z-index:999`
+        const randomColor =
+            this.options.palette[Math.floor(Math.random() * this.options.palette.length)]
+        //必须写成个函数每次调用一次保证得到不同的值
+        const randomTranslateValue = () =>
+            (Math.random() < 0.5 ? -1 : 1) *
+            (this.options.distanceMin +
+                Math.random() * (this.options.distanceMax - this.options.distanceMin))
+
         this.root.append(initialStyles)
         initialStyles.classList.add(this.options.type)
-        this.dynamic(initialStyles)
+        setTimeout(() => {
+            initialStyles.style.cssText = `background-color:#${randomColor};
+            left:${this.x}px;top:${this.y}px;
+            z-index:999;
+            transform: translate(${randomTranslateValue()}px,${randomTranslateValue()}px) scale(0)`
+        }, 0)
+        setTimeout(() => {
+            initialStyles.remove()
+        }, 700)
     }
 
     growUp() {
         for (let i = 0; i < this.options.quantity; i++) this.giveBirthToAChild()
-    }
-
-    mFmR(v) {
-        return Math.floor(Math.random() * v)
-    }
-
-    //随机散开
-    dynamic(x) {
-        setTimeout(() => {
-            x.style.transform = `translate(${this.openSwing()}px,${this.openSwing()}px) scale(0)`
-        })
-
-        setTimeout(() => {
-            x.remove()
-        }, 700)
     }
 }
 
